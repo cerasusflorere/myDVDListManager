2f44ee5060d069cf3281980cf389d559ec195bdf
cerasusflorere <cerasusflorere.180@gmail.com>
2023-11-24 18:46:51 +0900

    3.詳細追加登録-jsロジック

--- add.php
+++ add.php
@@ -256,6 +256,14 @@
                     </div>
                     <textarea id='add_story' class='add-story' placeholder="あらすじ"></textarea>
                 </div>
+
+                <!-- 脚本 -->
+                <div class='add-area add-author-area'>
+                    <div class='add-header'>
+                        作
+                    </div>
+                    <input type='text' id='add_author' class='add-author'>
+                </div>
                 
                 <!-- 出演者纏めて入力 -->
                 <div class='add-area add-players-all-area'>
@@ -453,7 +461,7 @@
                     <div class='add-header'>
                         歴史
                     </div>
-                    <textarea id='add_story' class='add-story' placeholder="本当は？"></textarea>
+                    <textarea id='add_history' class='add-story' placeholder="本当は？"></textarea>
                 </div>
 
                 <!-- その他 -->
@@ -551,10 +559,10 @@
                         カテゴリー
                     </div>
                     <select id='add_category' class='add-special-input add-special-input-text add-category'>
-                        <option value="1">いのうえ歌舞伎</option>
+                        <option value="1" selected>いのうえ歌舞伎</option>
                         <option value="2">ネタモノ</option>
                         <option value="3">音もの</option>
-                        <option value="4" selected>その他</option>
+                        <option value="4">その他</option>
                     </select>
                 </div>
             </div>
@@ -589,6 +597,7 @@
         let option_rolls_name = []; // 役名
         let select_rolls; // 役名を入力したら、役名のプルダウンメニューが増える。
         let select_rolls_name = []; // 選択されてる役名
+        let photos = [];
         const regex1 = /^[0-9]+$/; // 半角英数字入力のみに制限
         const regex2 = /^[ぁ-んー]+$/;
         const members = ['古田新太', '橋本じゅん', '高田聖子', '粟根まこと', '右近健一', '逆木圭一郎', '河野まさと', '村木よし子', 'インディ高橋', '山本カナコ', '礒野慎吾', '吉田メタル', '中谷さとみ', '保坂エマ', 'こぐれ修', '枯暮修', '竹田団吾', '羽野アキ', '羽野晶紀', '杉本恵美', 'タイソン大屋']; // 劇団員
@@ -643,12 +652,10 @@
             const id_number = id.replace('add_groups_group_', '');
             const inputGroup = event.currentTarget.value;
             option_groups_name[id_number - 1] = inputGroup;
-            console.log(option_groups_name);
             addOptionGroups();
         }
         function addOptionGroups(){
             select_groups = [].slice.call(document.querySelectorAll('[id^="add_players_group"]'));
-            console.log(select_groups);
             select_groups.forEach((select_group, number) => {
                 select_group.innerHTML = '';
                 let groupOption = document.createElement('option');
@@ -660,6 +667,33 @@
                     groupOption.setAttribute('text',name);
                     if(name == select_groups_name[number]){
                         groupOption.setAttribute('selected', 'true'); //プルダウンメニューで選択された名前だった場合selected
+                    }
+                    groupOption.innerHTML = name;
+                    select_group.appendChild(groupOption);
+                })
+            })
+        }
+        // 分類名が消えた時
+        function removeOptionGroups(removeGroup) {
+            let temp_option = option_groups_name.filter(function(name) {
+                return name !== removeGroup;
+            });
+            option_groups_name = temp_option;
+
+            select_groups = [].slice.call(document.querySelectorAll('[id^="add_players_group"]'));
+            select_groups.forEach((select_group, number) => {
+                select_group.innerHTML = '';
+                let groupOption = document.createElement('option');
+                groupOption.innerHTML = '選択';
+                select_group.appendChild(groupOption);
+                option_groups_name.forEach((name) => {
+                    let groupOption = document.createElement('option');
+                    groupOption.setAttribute('value',name);
+                    groupOption.setAttribute('text',name);
+                    if(name == select_groups_name[number]){
+                        groupOption.setAttribute('selected', 'true'); //プルダウンメニューで選択された名前だった場合selected
+                    } else if(select_groups_name[number] == removeGroup) {
+                        select_groups_name[number] = null;
                     }
                     groupOption.innerHTML = name;
                     select_group.appendChild(groupOption);
@@ -695,7 +729,33 @@
                     rollOption.setAttribute('value',name);
                     rollOption.setAttribute('text',name);
                     if(name == select_rolls_name[number]){
-                      rollOption.setAttribute('selected', 'true'); //プルダウンメニューで選択された名前だった場合selected
+                        rollOption.setAttribute('selected', 'true'); //プルダウンメニューで選択された名前だった場合selected
+                    }
+                    rollOption.innerHTML = name;
+                    select_roll.appendChild(rollOption);
+                })
+            })
+        }
+        function removeOptionRolls(removeRoll) {
+            let temp_option = option_rolls_name.filter(function(name) {
+                return name !== removeRoll;
+            });
+            option_rolls_name = temp_option;
+
+            select_rolls = [].slice.call(document.querySelectorAll('[id^="add_player_impressions_roll"]'));
+            select_rolls.forEach((select_roll, number) => {
+                select_roll.innerHTML = '';
+                let rollOption = document.createElement('option');
+                rollOption.innerHTML = '選択';
+                select_roll.appendChild(rollOption);
+                option_rolls_name.forEach((name) => {
+                    let rollOption = document.createElement('option');
+                    rollOption.setAttribute('value',name);
+                    rollOption.setAttribute('text',name);
+                    if(name == select_rolls_name[number]){
+                        rollOption.setAttribute('selected', 'true'); //プルダウンメニューで選択された名前だった場合selected
+                    } else if(select_rolls_name[number] == removeRoll) {
+                        select_rolls_name[number] = null;
                     }
                     rollOption.innerHTML = name;
                     select_roll.appendChild(rollOption);
@@ -703,7 +763,7 @@
             })
         }
 
-        // プルダウンメニューで選択された名前を取得
+        // プルダウンメニューで選択された役名を取得
         function chooseRoll(){
             const id = event.currentTarget.id;
             const id_number = id.replace('add_player_impressions_roll_', '');
@@ -726,35 +786,185 @@
                 document.getElementById('add_player_impressions_photo_preview_' + fileNumber).src = fileData.result;
             });
             fileData.readAsDataURL(photo_path.files[0]);
+            photos[fileNumber] = photo_path.files[0];
         };
 
         // document.querySelector("#add_player_impressions_photo_button_" + number).addEventListener("click", () => {
         //     document.querySelector("#add_player_impressions_photo_" + number).click();
         // });
 
-        
-        function submitData() {
-            let title = document.getElementById('add_title').value;
-            let title_kana = document.getElementById('add_title_kana').value;
-            console.log(title);
-            console.log(title_kana);
+        // データ送信
+        async function submitData() {
+            let sendData;
+            // データセット
+            let title;
+            let title_kana;
+            let duration_from;
+            let duration_to;
+            let locations = [];
+            let impression;
+            let story;
+            let author;
+            let groups = [];
+            let characters = [];
+            let history;
+            let others = [];
+            let official = true;
+            let special = false;
+            let eoshi;
+            let gekicine;
+            let category = 1;
+
+            // タイトル
+            title = document.getElementById('add_title').value;
+            title.trim();
+            title_kana = document.getElementById('add_title_kana').value;
+            title_kana.trim();
             if(!title && !title_kana){
                 return;
             }
-            let duration_from = document.getElementById('add_duration_from').value;
-            let duration_to = document.getElementById('add_duration_to').value;
 
-            let locations = [];
-            for(let i = 0; i < theater_number; i++){
+            // 公演期間
+            duration_from = document.getElementById('add_duration_from').value;
+            duration_to = document.getElementById('add_duration_to').value;
+
+            // 公演場所
+            k = 0;
+            for(let i = 0; i < location_number; i++){
                 let location_prefecture = document.getElementById('add_location_prefecture_' + (i + 1)).value;
-                if(!location_prefecture) {
-                    theater_number = theater_number - 1;
-                    return;
-                }
-                locations.push(Array(2));
-                locations[i][0] = parseInt(location_prefecture, 10);
-                locations[i][1] = document.getElementById('add_location_theater_' + (i + 1)).value;
+                let location_theater = document.getElementById('add_location_theater_' + (i + 1)).value;
+                location_theater.trim();
+                if(location_prefecture && location_prefecture != '選択' && location_theater) {
+                    locations[k] = [];
+                    locations[k].prefecture = parseInt(location_prefecture, 10);
+                    locations[k].theater = location_theater;
+                    k++;
+                }                 
             }
+
+            // 感想
+            impression = document.getElementById('add_impression').value;
+            impression.trim();
+
+            // あらすじ
+            story = document.getElementById('add_story').value;
+            story.trim();
+
+            // 脚本
+            author = document.getElementById('add_author').value;
+            author.trim();
+            author = author.replace(/\s+/g, "");
+
+            // 分類
+            for(let i = 0; i < group_number; i++) {
+                let group = document.getElementById('add_groups_group_' + (i + 1)).value;
+                groups.push(group.trim());
+            }
+
+            // 配役
+            k = 0;
+            for(let i = 0; i < player_number; i++) {
+                let player_group = document.getElementById('add_players_group_' + (i + 1)).value;
+                let player_roll = document.getElementById('add_players_roll_' + (i + 1)).value;
+                let player_player = document.getElementById('add_players_player_' + (i + 1)).value;
+                let player_member = document.getElementById('add_players_member_' + (i + 1)).checked;
+                player_group.trim();
+                player_roll.trim();
+                player_roll.replace(/\s+/g, "");
+                player_player.trim();
+                player_player.replace(/\s+/g, "");
+                if(player_roll && player_player) {
+                    console.log(k);
+                    if(!player_group || player_group == '選択') {
+                        player_group = null;
+                    }
+                    if(player_member) { 
+                        player_member = 1;
+                    } else {
+                        player_member = 0;
+                    }
+                    characters[k] = [];
+                    characters[k].group = player_group;
+                    characters[k].roll = player_roll;
+                    characters[k].player = player_player;
+                    characters[k].member = player_member;
+                    k++;
+                }
+            }
+
+            // 役感想
+            for(let i = 0; i < player_impression_number; i++) {
+                let player_impressions_roll = document.getElementById('add_player_impressions_roll_' + (i + 1)).value;
+                player_impressions_roll.trim();
+                player_impressions_roll.replace(/\s+/g, "");
+                let player_impressions_impression = document.getElementById('add_player_impressions_impression_' + (i + 1)).value;
+                player_impressions_impression.trim();
+                 
+                if(player_impressions_roll && player_impressions_roll != '選択' && player_impressions_impression) {
+                    const character_index = characters.findIndex((v) => v.roll === player_impressions_roll);
+                    if(character_index > -1) {
+                        characters[character_index].impression = player_impressions_impression;
+                        characters[character_index].photo = photos[i + 1];
+                    }
+                }
+            }
+
+            // 歴史
+            history = document.getElementById('add_history').value;
+            history.trim();
+
+            // その他
+            k = 0;
+            for(let i = 0; i < other_number; i++) {
+                let other_title = document.getElementById('add_others_title_' + (i + 1)).value;
+                other_title.trim();
+                let other_impression = document.getElementById('add_others_impression_' + (i + 1)).value;
+                other_impression.trim();
+
+                if(other_title && other_impression) { 
+                    others[k] = [];
+                    others[k].title = other_title;
+                    others[k].impression = other_impression;
+                    k++;
+                }
+            }
+
+            // 非公式
+            official = document.getElementById('add_official');
+
+            // 特典
+            special = document.getElementById('add_special');
+
+            // イーオシ
+            eoshi = document.getElementById('add_url_DVD').value;
+            eoshi.trim();
+
+            // ゲキ×シネ
+            gekicine = document.getElementById('add_url_movie').value;
+            gekicine.trim();
+
+            // カテゴリー
+            category = document.getElementById('add_category').value;
+
+            const formData = new FormData();
+            formData.append('title', title);
+            formData.append('title_kana', title_kana);
+            formData.append('duration_from', duration_from);
+            formData.append('duration_to', duration_to);
+            formData.append('locations', locations);
+            formData.append('impression', impression);
+            formData.append('story', story);
+            formData.append('author', author);
+            formData.append('groups', groups);
+            formData.append('characters', characters);
+            formData.append('history', history);
+            formData.append('others', others);
+
+            // const response = await axios.post('/api/add', formData);
+
+            // if (response.status !== 201) {
+            //     return false;
+            // }
 
         }
 
@@ -770,7 +980,7 @@
                 new_locations_line.id = 'add_location_' + location_number;
                 new_locations_line.getElementsByClassName('add-location-prefecture')[0].id = 'add_location_prefecture_' + location_number;
                 new_locations_line.getElementsByClassName('add-location-prefecture')[0].selectedIndex = 0;
-                new_locations_line.getElementsByClassName('add-location-theater')[0].id = 'add_location_theaterl_' + player_number;
+                new_locations_line.getElementsByClassName('add-location-theater')[0].id = 'add_location_theater_' + player_number;
                 new_locations_line.getElementsByClassName('add-location-theater')[0].value = null;
 
                 // 追加
@@ -859,6 +1069,10 @@
             if(player_number > 1) {
                 let players_bodyblock = document.getElementById('add_players_bodyblock');
                 let delete_players_bodyline = document.getElementById('add_players_bodyline_' + player_number);
+                let removeRoll = delete_players_bodyline.getElementsByClassName('add-players-roll')[0].value;
+                if(removeRoll && removeRoll != '選択') {
+                    removeOptionRolls(removeRoll);
+                }
 
                 // 削除
                 players_bodyblock.removeChild(delete_players_bodyline);
@@ -922,6 +1136,10 @@
             if(group_number > 1) {
                 let groups_bodyblock = document.getElementById('add_groups_bodyblock');
                 let delete_groups_bodyline = document.getElementById('add_groups_bodyline_' + group_number);
+                let removeGroup = delete_groups_bodyline.getElementsByClassName('add-groups-group')[0].value;
+                if(removeGroup && removeGroup != '選択') {
+                    removeOptionGroups(removeGroup);
+                }
 
                 // 削除
                 groups_bodyblock.removeChild(delete_groups_bodyline);
