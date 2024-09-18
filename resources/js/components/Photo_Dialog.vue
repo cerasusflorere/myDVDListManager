c4fcbab9d17ba3db707e50b3a1d32741b7e66153
cerasusflorere <cerasusflorere.180@gmail.com>
2024-09-18 17:43:37 +0900

    2.写真拡大

---
+++ resources/js/components/Photo_Dialog.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="overlay photo-overlay" @click.self="$emit('close')">
+    <div class="dialog photo-dialog">
+      <div class="message-button-area">
+        <button type="button" @click="$emit('close')" class="message-button"><i class="fas fa-times fa-fw"></i></button>
+      </div>
+      <div class="photo-area">
+        <figure class="photo-wrapper">
+          <img
+            class="photo-image"
+            :src="postURL"
+          >
+        </figure>
+      </div>
+    </div>
+  </div>
+</template>
+  
+<script>
+  export default {
+    // モーダルとして表示
+    name: 'photoDialog',
+    props: {
+      postURL: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
